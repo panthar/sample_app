9d14ae6b431b5b55bb9f3eda3c3aa74746f623d8
Scruffy Bob <panthar@gmail.com>
2011-03-15 11:14:27 -0500

    Added note to test rake notes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   # Return a title on a per-page basis.
+  # TODO: Make title generator just a bit more fancy
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
